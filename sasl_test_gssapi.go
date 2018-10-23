6758b7049c555b4719d8122c829f8c1af4d03f40
Jaume Marhuenda <jaumemarhuenda@gmail.com>
2018-10-22 21:09:24 -0700

    allow not to build kerberos

---
+++ sasl_test_gssapi.go
@@ -0,0 +1,30 @@
+// +build kerberos
+
+package gosasl
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+func TestGSSAPIMechanism(t *testing.T) {
+	mechanism, err := NewGSSAPIMechanism("hive")
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	client := NewSaslClient("localhost", mechanism)
+	client.GetConfig().AuthorizationID = "username"
+	client.Start()
+	for _, input := range [][]byte{[]byte("Ahjdskahdjkaw12kadlsj"), []byte("0"), nil} {
+		client.Step(input)
+	}
+
+	if client.Complete() {
+		t.Fatal("Client can't be complete")
+	}
+
+	client.Dispose()
+}
