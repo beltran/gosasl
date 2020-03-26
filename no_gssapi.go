6fcdddde6a0689c3688cd93eeea9a17d2229b07e
beltran <jaumemarhuenda@gmail.com>
2020-03-26 18:37:40 +0100

    update error message

--- no_gssapi.go
+++ no_gssapi.go
@@ -7,7 +7,7 @@ type GSSAPIMechanism struct {
 	host string
 }
 
-const errorMsg string = "gosasl was installed without kerberos support please reinstall with `go get` using the flags `build kerberos`"
+const errorMsg string = "gosasl may have been installed without kerberos support please reinstall with `go get` using the flags `build kerberos`. Alternatively if `go run` is being ran it should be ran with `go run -tags kerberos` and the binary should have been build with `go build -tags kerberos`."
 
 // NewGSSAPIMechanism returns a new GSSAPIMechanism
 func NewGSSAPIMechanism(service string) (mechanism *GSSAPIMechanism, err error) {
